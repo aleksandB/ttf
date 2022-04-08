b15e44bf3b35672f3771fa89cc1b887eca232c55
test_user <test_user@test_user.io>
2022-04-08 16:23:00 +0400

    add login function

--- ttf-vue-project/src/components/LoginPass.vue
+++ ttf-vue-project/src/components/LoginPass.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!isLoggedIn">
+    <div v-if="!loggedIn">
     <v-dialog max-width="400px" v-model="dialog">
         <template v-slot:activator="{ on, attrs}">
             <v-btn
@@ -17,9 +17,35 @@
             </v-card-title>
             <v-card-text>
                 <v-form class="px-3" ref="form">
-                    <v-text-field v-model="username" label="Login" prepend-icon="mdi-folder"></v-text-field>
-                    <v-text-field v-model="password" label="Password" prepend-icon="mdi-folder"></v-text-field>
-                    <v-btn @click="submit" class="success rounded-pill" :loading="loading">Login in</v-btn>
+                    <v-text-field 
+                        v-model="user.username" 
+                        label="Login" 
+                        prepend-icon="mdi-folder"
+                        v-validate="'required'"
+                        type="text"
+                        name="username"                            
+                    ></v-text-field>
+                    <div 
+                        v-if="errors.has('username')"
+                        class="alert alert-danger"
+                        role="alert"
+                    >Username is required!</div>
+                    <v-text-field v-model="user.password"
+                         label="Password"
+                         prepend-icon="mdi-folder"                         
+                         v-validate="'required'"
+                         type="text"
+                         name="password"
+                    ></v-text-field>
+                    <div 
+                        v-if="errors.has('password')"
+                        class="alert alert-danger"
+                        role="alert"
+                    >Password is required!</div>                    
+                    <v-btn @click="submit" class="form-group success rounded-pill" :loading="loading">Login in</v-btn>                    
+                    <div class="form-group">
+                    <div v-if="message" class="alert alert-danger" role="alert">{{message}}</div>
+                    </div>
                 </v-form>
             </v-card-text>
         </v-card>
@@ -40,10 +66,10 @@
                 <h2>Registration form</h2>
             </v-card-title>
             <v-card-text>
-                <v-form class="px-3" ref="form">
-                    <v-text-field v-model="username" label="Login" prepend-icon="mdi-folder"></v-text-field>
-                    <v-text-field v-model="password" label="Password" prepend-icon="mdi-folder"></v-text-field>
-                    <v-text-field v-model="password" label="Email" prepend-icon="mdi-folder"></v-text-field>
+                <v-form class="px-3">
+                    <v-text-field v-model="user.username" label="Login" prepend-icon="mdi-folder"></v-text-field>
+                    <v-text-field v-model="user.password" label="Password" prepend-icon="mdi-folder"></v-text-field>
+                    <v-text-field v-model="user.password" label="Email" prepend-icon="mdi-folder"></v-text-field>  <!-- Need change!-->
                     <v-btn @click="submit" class="success rounded-pill" :loading="loading">Sign up</v-btn>
                 </v-form>
             </v-card-text>
@@ -63,37 +89,84 @@
 </template>
 
 <script>
-export default {
+import User from '../models/user';
+export default {    
     data() {
         return {
-            form: {
-                username: '',
-                password: '',
-            },
+            user: new User('',''),            
             dialog: false,
             dialog2: false,            
             loading: false,
-            isLoggedIn: false            
+            message: ''               
         }
     },
     computed: {
-        
+        loggedIn() {
+            return this.$store.state.auth.status.loggedIn;
+        }
+    },
+    created() {
+        if (this.loggedIn) {
+            console.log('User logged In!'); //Will be changed after
+            this.$router.push('/');
+        }
     },
     methods: {
         submit() {
             console.log(this.login, this. password);
             this.dialog = false;
-            let username = this.username;
-            let password = this.password;
-            this.$store.dispatch('login', {username, password})
-                .then(() => 
-                    this.$router.push('/'))
-                .catch(err =>
-                    console.log(err))
+            this.$validator.validateAll().then(isValid => {
+                if(!isValid) {
+                    this.loading = false;
+                    return;
                 }
-            
+                if(this.user.username && this.user.password) {
+                    this.$store.dispatch('auth/login',this.user)
+                    .then(
+                        () => {
+                            this.$router.push('/');
+                        },
+                        error => {
+                            this.loading = false;
+                            this.message = 
+                            (error.response && error.response.data) || error.message || error.toString();
+                        }
+                    );
+                }
+            });
         }
-    
-
+    }
 }
 </script>
+
+<style scoped>
+label {
+  display: block;
+  margin-top: 10px;
+}
+.card-container.card {
+  max-width: 350px !important;
+  padding: 40px 40px;
+}
+.card {
+  background-color: #f7f7f7;
+  padding: 20px 25px 30px;
+  margin: 0 auto 25px;
+  margin-top: 50px;
+  -moz-border-radius: 2px;
+  -webkit-border-radius: 2px;
+  border-radius: 2px;
+  -moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+}
+.profile-img-card {
+  width: 96px;
+  height: 96px;
+  margin: 0 auto 10px;
+  display: block;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
+}
+</style>
