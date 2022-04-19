053337285465e78861714dd3a7e4d33cc149d038
test_user <test_user@test_user.io>
2022-04-19 16:07:51 +0400

    update frontend (add schedule scheme: add rules)

--- ttf-vue-project/src/components/NavBar.vue
+++ ttf-vue-project/src/components/NavBar.vue
@@ -6,9 +6,9 @@
         app
         class="secondary"
     >
-        <v-app-bar-nav-icon @click="drawer = !drawer" class="grey--text">            
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text">            
         </v-app-bar-nav-icon>
-        <v-toolbar-title>Title</v-toolbar-title>
+        <v-toolbar-title class="white--text">Title</v-toolbar-title>
         <v-spacer></v-spacer>
         <LoginPass />
     </v-app-bar>
@@ -17,10 +17,10 @@
            <v-list>
               <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
                 <v-list-item-action>
-                 <v-icon class="white--text">{{ link.icon }}</v-icon>
+                 <v-icon color="secondary">{{ link.icon }}</v-icon>
                 </v-list-item-action>
                 <v-list-item-content>
-                     <v-list-item-title class="grey--text">{{ link.text }}</v-list-item-title>
+                     <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
            </v-list>
@@ -39,9 +39,9 @@ export default {
         return {
             drawer: false,
             links: [
-            { icon: 'mdi-folder', text: 'ProfileView', route: '/profile' },        
-            { icon: 'mdi-folder', text: 'DashboardView', route: '/dashboard' },        
-            { icon: 'mdi-folder', text: 'ScheduleView', route: '/schedule' },
+            { icon: 'mdi-account', text: 'ProfileView', route: '/profile' },        
+            { icon: 'mdi-view-dashboard', text: 'DashboardView', route: '/dashboard' },        
+            { icon: 'mdi-puzzle', text: 'ScheduleView', route: '/schedule' },
         ],
         }
     }
--- ttf-vue-project/src/views/ScheduleView.vue
+++ ttf-vue-project/src/views/ScheduleView.vue
@@ -2,10 +2,10 @@
      <v-container class="cont__main">    
 
       <h2 class="text-h4 success--text pl-4">
-        Tasks:&nbsp;
+        Games:&nbsp;
         <v-fade-transition leave-absolute>
-          <span :key="`tasks-${tasks.length}`">
-            {{ tasks.length }}
+          <span :key="`games-${games.length}`">
+            {{ games.length }}
           </span>
         </v-fade-transition>
       </h2>
@@ -36,120 +36,112 @@
 
       <v-divider class="mb-4"></v-divider>
 
-      <v-card v-if="tasks.length > 0">
+      <v-card v-if="games.length > 0">
         
-          <template v-for="(task, i) in tasks">         
+          <template v-for="(game, i) in games">         
             <v-divider
               v-if="i !== 0"
               :key="`${i}-divider`"
             ></v-divider>
             
-            <v-list-item :key="`${i}-${task.text}`">
-              <v-list-item-action>
-                <v-checkbox
-                  v-model="task.done"
-                  :color="task.done && 'grey' || 'primary'"
-                >
-                  <template v-slot:label>
-                    <div
-                      :class="task.done && 'grey--text' || 'primary--text'"
-                      class="ml-4"
-                      v-text="task.text"
-                    ></div>
-                  </template>
-                </v-checkbox>
-              </v-list-item-action>
+            <v-list-item :key="`${i}-${game.game_id}`">
+              <v-list-item-action>               
+                    <v-row>
+                      <div class="table__names d-flex flex-column ma-5 my-auto">
+                          <div class="">
+                            <h3>Players</h3>
+                          </div>
+                          <div class="">
+                            {{ game.player_id1 }}
+                          </div>
+                          <div class="">
+                            {{ game.player_id2 }}
+                          </div>                
+                      </div>            
+                      <div v-if="game.game_total_player1 !== null" class="table__score d-flex flex-column ma-5 my-auto">
+                          <div class="">
+                            <h3>Total score</h3>
+                          </div>
+                          <div class="">
+                            {{ game.game_total_player1 }}
+                          </div>
+                          <div class="">
+                            {{ game.game_total_player2 }}
+                          </div>                
+                      </div>
+                      <div v-if="game.game_total_player1 !== null" class="table__round_ d-flex flex-column ma-5 my-auto">
+                            <h3>Score</h3>
+                          <div class="table__round d-flex flex-row ma-5 my-auto">
+                          <div 
+                          class="table__round d-flex flex-column my-auto"
+                          v-for="index in 5"
+                          :key="index"
+                          >
+                              <div v-if="[`game.score_set${index}_player1`]!==null" class="table__round2">
+                                {{  game[`score_set${index}_player1`] }}
+                              </div>
+                              <div class="table__round2">
+                                {{ game[`score_set${index}_player2`]}}
+                              </div>
+                          </div>                                               
+                      </div>
+                      </div>
+                      <div v-else class="table__round__tt d-flex flex-column ma-5 my-auto">
+                          <p>Waiting results</p>
+                      </div>
+                      <v-spacer/>
+                      <div v-if="!game.done && getCurrentUser(i)" class="table__btn d-flex flex-row justify-content-end ma-5 my-auto">  
+                            <v-tooltip top>
+                              <template v-slot:activator="{ on, attrs } ">
+                              <v-btn
+                              class="ma-1"  
+                              small                     
+                              color="primary"
+                              v-bind="attrs"
+                              v-on="on"
+                              >
+                                <v-icon fab centered>mdi-pencil</v-icon>
+                                <span class="caption text-lowercase"></span>
+                              </v-btn>                    
+                              </template>
+                              <span>Edit score</span>
+                            </v-tooltip>
+                            <v-tooltip top>
+                              <template v-slot:activator="{ on, attrs } ">
+                              <v-btn
+                              class="ma-1" 
+                              small                      
+                              color="info"
+                              v-bind="attrs"
+                              v-on="on"
+                              >
+                                <v-icon fab centered>mdi-package-down</v-icon>
+                                <span class="caption text-lowercase"></span>
+                              </v-btn>                    
+                              </template>
+                              <span>Save score</span>
+                            </v-tooltip>
+                        </div>    
+                        <div v-else-if="game.done" class="table__btn d-flex flex-row justify-content-end ma-5 my-auto">
+                          <v-spacer></v-spacer>
+                          <v-scroll-x-transition>
+                          <v-icon
+                            fab
+                            large                  
+                            color="success"
+                          >
+                            mdi-check
+                          </v-icon>
+                        </v-scroll-x-transition>
 
-              <v-spacer></v-spacer>
+                      </div>
+                    </v-row>
+              </v-list-item-action>              
 
-              <v-scroll-x-transition>
-                <v-icon
-                  v-if="task.done"
-                  color="success"
-                >
-                  mdi-check
-                </v-icon>
-              </v-scroll-x-transition>
+              
             </v-list-item>          
           </template>
-          <v-row>
-            <div class="table__names d-flex flex-column ma-5 my-auto">
-                <div class="">
-                  <h3>Puser1</h3>
-                </div>
-                <div class="">
-                  user1
-                </div>
-                <div class="">
-                  user2
-                </div>                
-            </div>            
-            <div class="table__score d-flex flex-column ma-5 my-auto">
-                <div class="">
-                  <h3>Scr</h3>
-                </div>
-                <div class="">
-                  3
-                </div>
-                <div class="">
-                  1
-                </div>                
-            </div>
-            <div class="table__round d-flex flex-column ma-5 my-auto">
-                  <h3>Scr</h3>
-                <div class="table__round d-flex flex-row ma-5 my-auto">
-                <div 
-                class="table__round d-flex flex-column my-auto"
-                v-for="index in 5"
-                :key="index"
-                >
-                    <div class="table__round2">
-                      3
-                    </div>
-                    <div class="table__round2">
-                      1
-                    </div>
-                </div>                                               
-            </div>
-            </div>
-            <v-spacer/>
-            <div class="table__btn d-flex flex-row justify-content-end ma-5 my-auto">  
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs } ">
-                    <v-btn 
-                    small 
-                    flat 
-                    color="primary"
-                    v-bind="attrs"
-                    v-on="on"
-                    >
-                      <v-icon small left>mdi-folder</v-icon>
-                      <span class="caption text-lowercase"></span>
-                    </v-btn>                    
-                    </template>
-                    <span>Edit score</span>
-                  </v-tooltip>
-                  <v-tooltip top>
-                    <template v-slot:activator="{ on, attrs } ">
-                    <v-btn 
-                    small 
-                    flat 
-                    color="primary"
-                    v-bind="attrs"
-                    v-on="on"
-                    >
-                      <v-icon small left>mdi-folder</v-icon>
-                      <span class="caption text-lowercase"></span>
-                    </v-btn>                    
-                    </template>
-                    <span>Save score</span>
-                  </v-tooltip>
-              
-            
-            </div>
-             
-
-          </v-row>
+         
 
        
       </v-card>
@@ -162,15 +154,61 @@ export default {
     name: 'ScheduleView',
     data() {
         return {
-            tasks: [
-        {
-          done: false,
-          text: 'Foobar',
-        },
-        {
-          done: false,
-          text: 'Fizzbuzz',
-        },
+            games: [
+                    {
+                      game_id: 1,
+                      player_id1: "aleksB",
+                      player_id2: "aleksB2",
+                      game_total_player1: null,
+                      game_total_player2: null,
+                      score_set1_player1: null,
+                      score_set1_player2: null,
+                      score_set2_player1: null,
+                      score_set2_player2: null,
+                      score_set3_player1: null,
+                      score_set3_player2: null,
+                      score_set4_player1: null,
+                      score_set4_player2: null,
+                      score_set5_player1: null,
+                      score_set5_player2: null,          
+                      done: false,          
+                    },
+                    {
+                      game_id: 2,
+                      player_id1: "aleksB",
+                      player_id2: "aleksB2",
+                      game_total_player1: 3,
+                      game_total_player2: 2,
+                      score_set1_player1: 2,
+                      score_set1_player2: 11,
+                      score_set2_player1: 11,
+                      score_set2_player2: 1,
+                      score_set3_player1: 11,
+                      score_set3_player2: 5,
+                      score_set4_player1: 3,
+                      score_set4_player2: 11,
+                      score_set5_player1: 13,
+                      score_set5_player2: 11,          
+                      done: true,          
+                    },
+                    {
+                      game_id: 3,
+                      player_id1: "aleksB3",
+                      player_id2: "aleksB",
+                      game_total_player1: 3,
+                      game_total_player2: 0,
+                      score_set1_player1: 13,
+                      score_set1_player2: 11,
+                      score_set2_player1: 11,
+                      score_set2_player2: 1,
+                      score_set3_player1: 11,
+                      score_set3_player2: 5,
+                      score_set4_player1: null,
+                      score_set4_player2: null,
+                      score_set5_player1: null,
+                      score_set5_player2: null,          
+                      done: false,          
+                    },
       ]
         };
     },
@@ -190,48 +228,74 @@ export default {
 
     computed: {
       completedTasks () {
-        return this.tasks.filter(task => task.done).length
+        return this.games.filter(game => game.done).length
       },
       progress () {
-        return this.completedTasks / this.tasks.length * 100
+        return this.completedTasks / this.games.length * 100
       },
-      remainingTasks () {
-        return this.tasks.length - this.completedTasks
+      remainingTasks () {        
+        return this.games.length - this.completedTasks
       },
+        
+     
     },
 
-    methods: {     
+    methods: {
+      getCurrentUser(index){
+        console.log(index)
+        console.log(this.games[index].player_id1)
+        if(this.games[index].player_id1 == this.$store.state.auth.user.username
+        || this.games[index].player_id2 == this.$store.state.auth.user.username) {
+          console.log("true")
+          return true;
+        } else {
+          console.log("false")
+          return false;
+        }
+      }       
+      
     },
 }
 </script>
 <style scoped>
 .cont__main {
-  max-width: 1200px;  
+  max-width: 900px;  
 }
 
 .table__names {
   width: 200px;
-  border: solid;
+  
   text-align: center;
 }
 .table__score {
-  width: 100px;
-  border: solid;
-  text-align: center;
-}
-.table__round {
   
-  border: solid;
+  width: 100px;
+  
   text-align: center;
 }
+.table__round_ {  
+  width: 230px;
+  
+  text-align: center;
+}
+.table__round {  
+ 
+  text-align: center;
+}
+.table__round__tt { 
+  width: 372px;
+  height: 40px; 
+  
+  text-align: center;
+}
+
 .table__round2{
   width: 32px;  
-  border: solid;
+  
   text-align: center;
 }
 .table__btn{
   width: 120px;  
-  border: solid;
   
 }
 
